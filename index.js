39f6fa98e4786c64692047c62155caa6f7a78bfd
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-09-30 17:06:31 -0400

    Added Tonal library

---
+++ index.js
@@ -0,0 +1,14 @@
+const getMidiNumbersFromNoteNames = notes => {
+
+}
+const sequence = {
+  quantizationInfo: {
+    stepsPerQuarter: 1
+  },
+  timeSignatures: [{
+    time: 0,
+    numerator: 3,
+    denominator: 4
+  }],
+  notes: []
+}
