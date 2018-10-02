864ec56dc3678d22e368e6cc32532196ade3aabd
Funmi Ojo <oluwafunmi.ojo@gmail.com>
2018-10-01 20:35:34 -0400

    Converted returned melody from midi, added setTimeout to play both original and generated melody

--- index.js
+++ index.js
@@ -4,30 +4,16 @@
 const improvCheckpoint = 'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
 const improvRNN = new mm.MusicRNN(improvCheckpoint)
 
-const synth = new Tone.DuoSynth().toMaster()
-const { Note } = Tonal
-
-const notes = [
-  'F4', 'G4', 'G#4', 'C5', 'D#5', 'C#5', 'C5', 'G#4', 'F4', 'D#5', 'C5', 'B4', 'E5', 'B4', 'G4', 'E4', 'A5', 'C5', 'B5', 'F5', 'C5', 'B4', 'G4', 'D#4', 'G4', 'E4', 'E5', 'C#5', 'C5', 'E5', 'C5', 'B4', 'G4', 'A#4', 'G4', 'B4', 'G4', 'D#4', 'G4', 'E4', 'C4', 'B3'
-  ]
-
-const notesWithoutOctaves = notes.map(note => note.slice(0, note.length - 1))
-//console.log(notesWithoutOctaves)
-const notesWithNoRepeats = notesWithoutOctaves.filter((note, index) => {
-  notesWithoutOctaves.indexOf(note) === index
-})
-
-// console.log(notes.map((note, index) => {
-//     return { pitch: Note.midi(note), quantizedStartStep: index * 0.5, quantizedEndStep: (index + 1) * 0.5 }
-//     }))
+const synth = new Tone.Synth().toMaster()
+const { midi, Note } = Tonal
 
 const sequence = {
   ticksPerQuarter: 220,
-  totalTime: notes.length * 0.5,
+  totalTime: 58,
   timeSignatures: [
     {
       time: 0,
-      numerator: 3,
+      numerator: 4,
       denominator: 4
     }
   ],
@@ -37,30 +23,59 @@ const sequence = {
       qpm: 120
     }
   ],
-  notes: notes.map((note, index) => {
-    return { pitch: Note.midi(note), startTime: index * 0.5, endTime: (index + 1) * 0.5 }
-    })
+  notes: [
+    { pitch: midi('Gb4'), startTime: 0, endTime: 1 },
+    { pitch: midi('F4'), startTime: 1, endTime: 3.5 },
+    { pitch: midi('Ab4'), startTime: 3.5, endTime: 4 },
+    { pitch: midi('C5'), startTime: 4, endTime: 4.5 },
+    { pitch: midi('Eb5'), startTime: 4.5, endTime: 5 },
+    { pitch: midi('Gb5'), startTime: 5, endTime: 6 },
+    { pitch: midi('F5'), startTime: 6, endTime: 7 },
+    { pitch: midi('E5'), startTime: 7, endTime: 8 },
+    { pitch: midi('Eb5'), startTime: 8, endTime: 8.5 },
+    { pitch: midi('C5'), startTime: 8.5, endTime: 9 },
+    { pitch: midi('G4'), startTime: 9, endTime: 11.5 },
+    { pitch: midi('F4'), startTime: 11.5, endTime: 12 },
+    { pitch: midi('Ab4'), startTime: 12, endTime: 12.5 },
+    { pitch: midi('C5'), startTime: 12.5, endTime: 13 },
+    { pitch: midi('Eb5'), startTime: 13, endTime: 14 },
+    { pitch: midi('D5'), startTime: 14, endTime: 15 },
+    { pitch: midi('Db5'), startTime: 15, endTime: 16 },
+    { pitch: midi('C5'), startTime: 16, endTime: 16.5 },
+    { pitch: midi('F5'), startTime: 16.5, endTime: 17 },
+    { pitch: midi('F4'), startTime: 17, endTime: 19.5 },
+    { pitch: midi('G4'), startTime: 19.5, endTime: 20 },
+    { pitch: midi('Ab4'), startTime: 20, endTime: 20.5 },
+    { pitch: midi('C5'), startTime: 20.5, endTime: 21 },
+    { pitch: midi('Eb5'), startTime: 21, endTime: 21.5 },
+    { pitch: midi('C5'), startTime: 21.5, endTime: 22 },
+    { pitch: midi('Eb5'), startTime: 22, endTime: 22.5 },
+    { pitch: midi('C5'), startTime: 22.5, endTime: 24.5 },
+    { pitch: midi('Eb5'), startTime: 24.5, endTime: 25.5 },
+    { pitch: midi('G4'), startTime: 25.5, endTime: 28.5 }
+  ]
 }
 
-const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 4)
+const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 1)
+
 const startProgram = async () => {
   try {
-    //await melodyRNN.initialize()
-    console.log('improv', melodyRNN)
-    //let generatedMelody = await melodyRNN.continueSequence(quantizedSequence, 80)
-    //console.log('generatedMelody', generatedMelody)
-    //notes.forEach((note, index) => synth.triggerAttackRelease(note, 0.5, index))
-    //generatedMelody.notes.forEach((note, index) => synth.triggerAttackRelease(note.pitch, 0.5, index))
     await improvRNN.initialize()
-    let improvisedMelody = await improvRNN.continueSequence(quantizedSequence, 160, 1.1, ['Dm', 'Cm', 'Em', 'Gm'])
+    let improvisedMelody = await improvRNN.continueSequence(quantizedSequence, 60, 1.1, ['Bm', 'Bbm', 'Gb7', 'F7', 'Ab', 'Ab7', 'G7', 'Gb7', 'F7', 'Bb7', 'Eb7', 'AM7'])
     console.log('improvisedMelody', improvisedMelody)
-    improvisedMelody.notes.forEach((note, index) => {
-      synth.triggerAttackRelease(Note.fromMidi(note.pitch), '4n', index)
+    sequence.notes.forEach(note => {
+      synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.endTime - note.startTime, note.startTime)
     })
+
+    setTimeout(() => {
+      improvisedMelody.notes.forEach(note => {
+        synth.triggerAttackRelease(Note.fromMidi(note.pitch), note.quantizedEndStep - note.quantizedStartStep, note.quantizedStartStep)
+      })
+    }, 2000)
+
   } catch (error) {
     console.error(error)
   }
 }
 
-console.log(notes.length)
 startProgram()
